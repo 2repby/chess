b2ea20b18dab291c063b3bf041ea2e160536c3e9
kda <2repby@gmail.com>
2023-04-04 19:36:36 +0500

    class Knight

---
+++ main.py
@@ -0,0 +1,246 @@
+WHITE = 1
+BLACK = 2
+
+class Pawn:
+
+    def __init__(self, row, col, color):
+        self.row = row
+        self.col = col
+        self.color = color
+
+    def set_position(self, row, col):
+        self.row = row
+        self.col = col
+
+    def char(self):
+        return 'P'
+
+    def get_color(self):
+        return self.color
+
+    def can_move(self, row, col):
+        # Пешка может ходить только по вертикали
+        # "взятие на проходе" не реализовано
+        if self.col != col:
+            return False
+
+        # Пешка может сделать из начального положения ход на 2 клетки
+        # вперёд, поэтому поместим индекс начального ряда в start_row.
+        if self.color == WHITE:
+            direction = 1
+            start_row = 1
+        else:
+            direction = -1
+            start_row = 6
+
+        # ход на 1 клетку
+        if self.row + direction == row:
+            return True
+
+        # ход на 2 клетки из начального положения
+        if self.row == start_row and self.row + 2 * direction == row:
+            return True
+
+        return False
+
+class Rook:
+
+    def __init__(self, row, col, color):
+        self.row = row
+        self.col = col
+        self.color = color
+
+    def set_position(self, row, col):
+        self.row = row
+        self.col = col
+
+    def char(self):
+        return 'R'
+
+    def get_color(self):
+        return self.color
+
+    def can_move(self, row, col):
+        # Невозможно сделать ход в клетку, которая не лежит в том же ряду
+        # или столбце клеток.
+        if self.row != row and self.col != col:
+            return False
+
+        return True
+
+class Knight:
+
+    def __init__(self, row, col, color):
+        self.row = row
+        self.col = col
+        self.color = color
+
+    def set_position(self, row, col):
+        self.row = row
+        self.col = col
+
+    def char(self):
+        return 'N'
+
+    def get_color(self):
+        return self.color
+
+    def can_move(self, row, col):
+        if not correct_coords(row, col):
+            return False
+        row1, col1 = self.row + 2, self.col + 1
+        if correct_coords(row1, col1):
+            if row == row1 and col == col1:
+                return True
+        row1, col1 = self.row + 2, self.col - 1
+        if correct_coords(row1, col1):
+            if row == row1 and col == col1:
+                return True
+        row1, col1 = self.row - 2, self.col + 1
+        if correct_coords(row1, col1):
+            if row == row1 and col == col1:
+                return True
+        row1, col1 = self.row - 2, self.col - 1
+        if correct_coords(row1, col1):
+            if row == row1 and col == col1:
+                return True
+
+        row1, col1 = self.row + 1, self.col + 2
+        if correct_coords(row1, col1):
+            if row == row1 and col == col1:
+                return True
+        row1, col1 = self.row + 1, self.col - 2
+        if correct_coords(row1, col1):
+            if row == row1 and col == col1:
+                return True
+        row1, col1 = self.row - 1, self.col + 2
+        if correct_coords(row1, col1):
+            if row == row1 and col == col1:
+                return True
+        row1, col1 = self.row - 1, self.col - 2
+        if correct_coords(row1, col1):
+            if row == row1 and col == col1:
+                return True
+
+        return False
+
+class Board:
+    def __init__(self):
+        self.color = WHITE
+        self.field = []
+        for row in range(8):
+            self.field.append([None] * 8)
+        # Пешка белого цвета в клетке E2.
+        self.field[1][4] = Pawn(1, 4, WHITE)
+
+    def current_player_color(self):
+        return self.color
+
+    def cell(self, row, col):
+        """Возвращает строку из двух символов. Если в клетке (row, col)
+        находится фигура, символы цвета и фигуры. Если клетка пуста,
+        то два пробела."""
+        piece = self.field[row][col]
+        if piece is None:
+            return '  '
+        color = piece.get_color()
+        c = 'w' if color == WHITE else 'b'
+        return c + piece.char()
+
+    def move_piece(self, row, col, row1, col1):
+        """Переместить фигуру из точки (row, col) в точку (row1, col1).
+        Если перемещение возможно, метод выполнит его и вернет True.
+        Если нет --- вернет False"""
+
+        if not correct_coords(row, col) or not correct_coords(row1, col1):
+            return False
+        if row == row1 and col == col1:
+            return False  # нельзя пойти в ту же клетку
+        piece = self.field[row][col]
+        if piece is None:
+            return False
+        if piece.get_color() != self.color:
+            return False
+        if not piece.can_move(row1, col1):
+            return False
+        self.field[row][col] = None  # Снять фигуру.
+        self.field[row1][col1] = piece  # Поставить на новое место.
+        piece.set_position(row1, col1)
+        self.color = opponent(self.color)
+        return True
+
+# Удобная функция для вычисления цвета противника
+def opponent(color):
+    if color == WHITE:
+        return BLACK
+    return WHITE
+
+def correct_coords(row, col):
+    """Функция проверяет, что координаты (row, col) лежат
+    внутри доски"""
+    return 0 <= row < 8 and 0 <= col < 8
+
+def print_board(board):  # Распечатать доску в текстовом виде (см. скриншот)
+    print('     +----+----+----+----+----+----+----+----+')
+    for row in range(7, -1, -1):
+        print(' ', row, end='  ')
+        for col in range(8):
+            print('|', board.cell(row, col), end=' ')
+        print('|')
+        print('     +----+----+----+----+----+----+----+----+')
+    print(end='        ')
+    for col in range(8):
+        print(col, end='    ')
+    print()
+
+def main():
+    # Создаём шахматную доску
+    board = Board()
+    # Цикл ввода команд игроков
+    while True:
+        # Выводим положение фигур на доске
+        print_board(board)
+        # Подсказка по командам
+        print('Команды:')
+        print('    exit                               -- выход')
+        print('    move <row> <col> <row1> <col1>     -- ход из клетки (row, col)')
+        print('                                          в клетку (row1, col1)')
+        # Выводим приглашение игроку нужного цвета
+        if board.current_player_color() == WHITE:
+            print('Ход белых:')
+        else:
+            print('Ход черных:')
+        command = input()
+        if command == 'exit':
+            break
+        move_type, row, col, row1, col1 = command.split()
+        row, col, row1, col1 = int(row), int(col), int(row1), int(col1)
+        if board.move_piece(row, col, row1, col1):
+            print('Ход успешен')
+        else:
+            print('Координаты некорректы! Попробуйте другой ход!')
+
+
+# main()
+
+row0_W = 0
+col0_W = 1
+knight_W = Knight(row0_W, col0_W, WHITE)
+
+row0_B = 7
+col0_B = 6
+knight_B = Knight(row0_B, col0_B, BLACK)
+
+print('white' if knight_W.get_color() == WHITE else 'black')
+print('white' if knight_B.get_color() == WHITE else 'black')
+for row in range(7, -1, -1):
+    for col in range(8):
+        if (row0_W, col0_W) == (row, col):
+            print(knight_W.char(), end='')
+        elif (row0_B, col0_B) == (row, col):
+            print(knight_B.char(), end='')
+        elif knight_W.can_move(row, col) or knight_B.can_move(row, col):
+            print('x', end='')
+        else:
+            print('-', end='')
+    print()
